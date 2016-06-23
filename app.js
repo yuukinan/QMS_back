7e0244c9b3c20af62d9da9182ff788a6639cc699
originalsoul <originalsoul@yeah.net>
2016-06-23 16:38:39 +0800

    add list

--- app.js
+++ app.js
@@ -1,55 +1,66 @@
 var model = require('./models')
-var express = require('express');
-var path = require('path');
+var express = require('express')
+var path = require('path')
 var bodyParser = require('body-parser')
 
-var app = express();
+var app = express()
 
-//app.use(express.static(path.join(_dirname, 'public'))) 
-app.use(express.static('public'));
-//parse application/x-www-form-urlencoded 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'))
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 // parse application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-//app.get('/', function(req, res){
-// 	res.send('Hello World!');
-// });
 
-//app.post('/addName',function(req,res){
-//    console.log('req: ',req)
-//    console.log(req.body)
-//      res.send('hello world')
-//})
 app.get('/detail',function(req,res){
 
 })
+
 app.get('/list',function(req,res){
-
-})
-app.post('/addNew',function(req,res){
-	console.log(req.body)
-
-	model.question.create(req.body).then(function(val){
-	console.log(val)
-	//res.json({
-	//  data:{
-	//  },
-	//  result: {
-	//    code: 200,
-	//    msg: "success"}
-	//    })
-}, function (err){
-	res.json({
-		result: {
-			code: 201,
-			mesg: "fail"
-		}
+	model.question.findAll({
+		order: 'id DESC'
+	}).then(function (data) {
+		console.log(data)
+		res.json({
+			data: {
+				questionList: data
+			},
+			result: {
+				code: 200,
+				msg: "success"
+			}
+		})
+	}, function (err) {
+		res.json({
+			result: {
+				code: 201,
+				msg: "fail"
+			}
+		})
 	})
-	console.log(err)
 })
 
+app.post('/addNew',function(req,res){
+	model.question.create(req.body).then(function(val){
+		res.json({
+		 data:{
+		 	id: val.dataValues.id
+		 },
+		 result: {
+		   code: 200,
+		   msg: "success"}
+		   })
+	}, function (err){
+		res.json({
+			result: {
+				code: 201,
+				mesg: "fail"
+			}
+		})
+	})
 })
+
 app.post('/edit',function(req,res){
 
 })
